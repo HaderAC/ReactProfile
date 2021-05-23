7fc9d711ca71ed4931ba20fab50adcd7e864f9ea
HaderCore <Hadercortes@gmail.com>
2021-05-23 00:18:01 -0400

    created navigation bar

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">HADER CORTES</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link" href="/contact">Contact</a>
+                <a class="nav-link" href="/portfolio">Portfolio</a>
+            </div>
+        </div>
+    </nav>
+    );
+};
+
+export default Header;

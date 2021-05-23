83bf15f9471c0b805d68294c1002fe15c13fb906
HaderCore <Hadercortes@gmail.com>
2021-05-22 23:18:41 -0400

    Pages routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import {HashRouter, Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import Portfolio from "./Pages/Portfolio";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
-    <div>
-     
-    </div>
+   <HashRouter basename={process.env.PUBLIC_URL}>
+     <Route exact path="/" component={Home}/>
+     <Route exact path="/portfolio" component={Portfolio}/>
+     <Route exact path="/contact" component={Contact}/>
+   </HashRouter>
   );
 }
 

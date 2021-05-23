07cb18d396d5aa5e982ded6780319c5d1db6af7f
HaderCore <Hadercortes@gmail.com>
2021-05-23 00:16:03 -0400

    Added Header to app as well as imported it

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Home from "./Pages/Home";
 import Portfolio from "./Pages/Portfolio";
 import Contact from "./Pages/Contact";
 import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 function App() {
   return (
    <HashRouter basename={process.env.PUBLIC_URL}>
+     <Header/>
      <Route exact path="/" component={Home}/>
      <Route exact path="/portfolio" component={Portfolio}/>
      <Route exact path="/contact" component={Contact}/>

a5270ae7a47245f532fc397232c42b51794a5652
HaderCore <Hadercortes@gmail.com>
2021-05-24 20:33:51 -0400

    react portfolio Header COmponents

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">HADER CORTES</a>
+        <Link class="navbar-brand" to="/">HADER CORTES</Link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link" href="/contact">Contact</a>
-                <a class="nav-link" href="/portfolio">Portfolio</a>
+                <Link class="nav-link" to="/contact">Contact</Link>
+                <Link class="nav-link" to="/portfolio">Portfolio</Link>
+                <Link class="nav-link" to="/resume">Resume</Link>
             </div>
         </div>
     </nav>

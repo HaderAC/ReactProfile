15f57408d081c34f033e0ec736a84830e9ff2950
HaderCore <Hadercortes@gmail.com>
2021-05-24 20:35:27 -0400

    home pages edited and worked on

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -17,49 +17,19 @@ const Home = () => {
                             <p className="card-text"> Was born in colombia 35 years ago and for the last 10 years has been working
                             in T.V production. His curiosity and willingless to adapt is what lets him find new, innovative
                             ways to accomplish tasks. Most of all he likes to create things he's proud of and hopes you'll let
-                    him be a part of your dream.</p>
+                            him be a part of your dream.</p>
+                            
+                            <div className="card-body">
+                                <p className="card-text">
+                                    lkasdhfliadjhf;aksdfja ;asdfhj jsadflkashjfd lkjsadfh ljksadfhl jsdflkj faslkjfdh lsadjkfh  kjlsdsa f hsldj ds
+                                    asdfgsdf adfasdf asdfads asdfasd asdff asdfasdg afdg sdaf asf dsa gdfsag sd fasdf dsaf ad f asdfasd fasd
+                                    sfdgdfgsd
+                                    😆
+                        </p>
+                            </div>
                         </div>
                     </article>
                 </div>
-            </section>
-
-
-            <section id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img className="d-block w-25"
-                            src="./Screenshots/PP.png"
-                            alt="Coming soon..." />
-                        <p>First Project</p>
-                    </div>
-                    <div className="carousel-item">
-                        <img className="d-block w-25"
-                            src="./Screenshots/Moods.png"
-                            alt="Coming soon..." />
-                        <p>Second Project</p>
-                    </div>
-                    <div className="carousel-item">
-                        <img className="d-block w-25"
-                            src="./Screenshots/codePub.png"
-                            alt="Coming soon..." />
-                        <p>Third Project</p>
-                    </div>
-                </div>
-                <a className="carousel-control-prev"  role="button" data-slide="prev">
-                    <i className="fas fa-arrow-left"></i>
-                    {/* <span className="carousel-control-prev-icon" aria-hidden="true"></span> */}
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" role="button" data-slide="next">
-                    <i className="fas fa-arrow-right"></i>
-                    {/* <span className="carousel-control-next-icon" aria-hidden="true"></span> */}
-                    <span className="sr-only">Next</span>
-                </a>
             </section>
 
 

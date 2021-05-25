71e7e04a6201777931ac503a9ae18339dfbb117b
HaderCore <Hadercortes@gmail.com>
2021-05-24 20:36:00 -0400

    added resume and fixed some styling

---
+++ src/Pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const resume = () => {
+    return (
+        <div>
+            <iframe src="https://drive.google.com/file/d/1GJrDeKci1v0bAGp-EvzLJHRqC6p5SB8J/preview"/>
+        </div>
+    );
+};
+
+export default resume;
